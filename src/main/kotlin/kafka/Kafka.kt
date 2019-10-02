b14e982ba0cabf4404051ffcc281d46ba945ab8a
oem <sunnybunny14@gmail.com>
2019-10-02 14:26:57 +0300

    README file added

--- src/main/kotlin/kafka/Kafka.kt
+++ src/main/kotlin/kafka/Kafka.kt
@@ -3,7 +3,7 @@ package kafka
 data class Kafka(val bootstrapServers: String)  {
 
     companion object    {
-        const val CREDIT_CARDS_TOPIC = "creditcards"
+        const val CREDIT_CARDS_TOPIC = "creditcard"
         const val IBAN_TOPIC = "iban"
         const val BROCKERS = "localhost:9092"
     }
